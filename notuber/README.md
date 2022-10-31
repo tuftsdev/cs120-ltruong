2561d404953388fda3f40d26c7044a23505e7f48
Loi Truong <loitruong1989@gmail.com>
2022-10-31 07:22:19 -0400

    Ride-Hailing Service - Part 2
    Edit JavaScript to request rides from the rides API.
    Add click event listener to user marker.
    get the shortest distance ride by using Haversine formula.
    Connect the user marker with the shortest ride.

--- notuber/README.md
+++ notuber/README.md
@@ -1,29 +1,12 @@
-# About LAB 8 - The Ride-Hailing Service - Part 1
-  Using google API to display the map and add markers with customizing car icon.
+# About LAB 8 - The Ride-Hailing Service - Part 2
+  The project uses google API and Rides API to get the closest ride—the calculation between the user and rides using the Haversine formula. To get the ride, the user must allow the app to get the location and click on its marker to reveal the shortest ride.
 
 # Collaborators
   - [Loi Truong](https://github.com/loitruong)
 
-
-# Performance Optimization
-Using regular 4G for thorrling
-  - Before Optimize:
-    - 1.54 MB Resources 
-    - style.css = 89 B
-    - script.js = 963 B
-    - 10 hard refresh with average of 6.868s
-
-  - After Optimize:
-    - 1.54 MB Resources
-    - style.css = 67 B
-    - script.js = 604 B
-    - 10 hard refresh with average of 6.937s
-
-  - The most significant bottleneck is loading external google Maps javascript files. Although I make the files smaller than before, these are local files, so it only takes 40ms-80ms to load each file, which is way too small to see any difference. Maybe the performance will be different if the CSS/js files have come from another server instead of localhost.
-
   
 # Number of working hours
-  2.5 hours
+  4 hours
 
 
 
--- notuber/script.js
+++ notuber/script.js
@@ -1,63 +1,162 @@
-let map;
+(function() {
+    "user strict";
 
-let markers = [{
-  vid : "mXfkjrFw",
-  lat: 42.3453,
-  lng: -71.0464
-},
-{
-  vid : "nZXB8ZHz",
-  lat: 42.3662,
-  lng: -71.0621
-},
-{
-  vid : "Tkwu74WC",
-  lat: 42.3603,
-  lng: -71.0547
-},
-{
-  vid : "5KWpnAJN",
-  lat: 42.3472,
-  lng: -71.0802
-},
-{
-  vid : "uf5ZrXYw",
-  lat: 42.3663,
-  lng: -71.0544
-},
-{
-  vid : "mXfkjrFw",
-  lat: 42.3542,
-  lng: -71.0704
-}]
+    let map;
+    let userGMarker;
+    let userPos;
+    const mapCenter = { lat: 42.352271, lng: -71.05524200000001 };
 
 
-function initMap() {
-  
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 42.352271, lng: -71.05524200000001}
-  });
+    function getUserLocation() {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+        	userPos = pos;
+            if (map) {
+                loadRides();
 
-  //set bounds
-  let bounds = new google.maps.LatLngBounds();
+            }
+        }, function(error) {
+            console.error(error);
+        }, {
+            enableHighAccuracy: true,
+            maximumAge: 0
+        });
+    }
 
-  for (const marker of markers){
-    let gmarker = new google.maps.Marker({
-      position: {
-        lat: marker.lat,
-        lng: marker.lng
-      },
-      map,
-      title: marker.vid,
-      icon: "car.png"
-    });
-    bounds.extend(gmarker.getPosition());
-  }
+    function addUserMarkerInfo(rideGMarker){
+    	infoWindowHtml =  `<p>Your Ride: ${rideGMarker.title} <br/>Distance: ${rideGMarker.distance} miles</p>`;
+    	userGMarker['infowindow'] = new google.maps.InfoWindow({
+    	        content: infoWindowHtml
+    	    });
 
-  map.fitBounds(bounds);
+    	google.maps.event.addListener(userGMarker, 'click', function() {
+    	    this['infowindow'].open(map, this);
+    	    connectRide(rideGMarker);
+    	    fitBoundMarkers([userGMarker, rideGMarker]);
+    	});
+    }
+
+    function connectRide(rideGMarker){
+    	const path = new google.maps.Polyline({
+    	  path: [
+			{"lat": userGMarker.position.lat(), "lng": userGMarker.position.lng()},
+			{"lat": rideGMarker.position.lat(), "lng": rideGMarker.position.lng()},
+    	  ],
+    	  geodesic: true,
+    	  strokeColor: "#FF0000",
+    	  strokeOpacity: 1.0,
+    	  strokeWeight: 2,
+    	});
+
+    	path.setMap(map);
+    }
+
+    function addMarker(lat, lng, title, icon) {
+        let gmarker = new google.maps.Marker({
+            position: {
+                lat: lat,
+                lng: lng
+            },
+            map,
+            title: title,
+            icon: icon
+        });
+        return gmarker;
+    }
 
 
-}
+    function fitBoundMarkers(markers){
+    	//set bounds
+    	let bounds = new google.maps.LatLngBounds();
+
+    	for (const marker of markers) {
+    	    bounds.extend(marker.getPosition());
+    	}
+
+    	map.fitBounds(bounds);
+    }
 
 
-window.initMap = initMap;
+    function loadRides() {
+    	if(!userPos) return;
+
+        //google map loaded so let add the user position in google map
+        userGMarker = addMarker(userPos.coords.latitude, userPos.coords.longitude, "", "");
+        fitBoundMarkers([userGMarker]);
+
+        let xhr = new XMLHttpRequest();
+        let userPosition = userGMarker.getPosition();
+        const URL = "https://jordan-marsh.herokuapp.com/rides";
+        const USERNAME = "jLttbNzY";
+        const PARAMS = `username=${USERNAME}&lat=${userGMarker.position.lat()}&lng=${userGMarker.position.lng()}`;
+        xhr.open("POST", URL);
+
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                let response = xhr.responseText;
+                let rides = [];
+                try {
+                    rides = JSON.parse(response);
+                } catch (e) {
+                    console.error(e.message);
+                }
+                let shortestGMarker = null;
+                for (const ride of rides) {
+                	// add marker
+                    gMarker = addMarker(ride["lat"], ride["lng"], ride["username"], "car.png");
+                    gMarker["distance"] = getDistance({"lat": userGMarker.position.lat(), "lng": userGMarker.position.lng()}, ride);
+                    if( shortestGMarker == null || gMarker["distance"] < shortestGMarker["distance"]){
+                    	shortestGMarker = gMarker;
+                    }
+                }
+                //Add information
+                addUserMarkerInfo(shortestGMarker);
+            }
+        }
+
+        xhr.send(PARAMS);
+    }
+
+
+    function getDistance(pos1, pos2){
+    	const earthR =  3958.756; //in miles
+    	let toRadius = (value) => { return value * Math.PI / 180;};
+    	let lat1R = toRadius(pos1.lat);
+    	let lat2R = toRadius(pos2.lat);
+    	let deltaLatR = toRadius(pos2.lat - pos1.lat);
+    	let deltaLngR = toRadius(pos2.lng - pos1.lng);
+
+    	let a = Math.sin(deltaLatR/2) * Math.sin(deltaLatR/2) +
+    			Math.cos(lat1R) * Math.cos(lat2R) *
+    			Math.sin(deltaLngR/2) * Math.sin(deltaLngR/2);
+
+    	let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    	return earthR * c;
+    }
+
+
+    function initMap() {
+
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: mapCenter,
+            zoom: 15
+        });
+        // User Info loaded first
+        if(userPos){
+        	//userPos loaded first
+        	userGMarker = addMarker(userPos.coords.latitude, userPos.coords.longitude, "", "");
+        	fitBoundMarkers([userGMarker]);
+        	loadRides();
+        }
+    }
+	
+    getUserLocation();
+    window.initMap = initMap;
+
+
+
+
+
+}());
